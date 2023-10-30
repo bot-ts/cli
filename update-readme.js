6f0345d447c8f6a8e1ec236bae2ee31c7f9a57b1
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-10-30 14:38:32 +0100

    added a readme updater

---
+++ update-readme.js
@@ -0,0 +1,23 @@
+const axios = require("axios")
+const fs = require("fs")
+const packageJSON = require("./package.json")
+
+axios
+  .get(
+    "https://raw.githubusercontent.com/bot-ts/.github/main/profile/readme.md"
+  )
+  .then((response) => {
+    fs.writeFile("readme.md", response.data.replace(
+      /<div class="title"><\/div>/,
+      `<h1> ${packageJSON.name} </h1><p> ${packageJSON.description} </p>`
+    ), (err) => {
+      if (err) {
+        console.error("Error writing readme.md:", err)
+      } else {
+        console.log("readme.md updated successfully.")
+      }
+    })
+  })
+  .catch((error) => {
+    console.error("Error downloading readme.md:", error)
+  })
