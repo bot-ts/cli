3411d56e4ead073c3f33054b651be465c81c6123
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-10-31 11:42:20 +0100

    published 7.0.3 for Senchu release

--- update-readme.js
+++ update-readme.js
@@ -7,16 +7,20 @@ axios
     "https://raw.githubusercontent.com/bot-ts/.github/main/profile/readme.md"
   )
   .then((response) => {
-    fs.writeFile("readme.md", response.data.replace(
-      /<div class="title"><\/div>/,
-      `<h1> ${packageJSON.name} </h1><p> ${packageJSON.description} </p>`
-    ), (err) => {
-      if (err) {
-        console.error("Error writing readme.md:", err)
-      } else {
-        console.log("readme.md updated successfully.")
+    fs.writeFile(
+      "readme.md",
+      response.data.replace(
+        /<div class="title"><\/div>/,
+        `<h1> ${packageJSON.name} </h1><p> ${packageJSON.description} </p>`
+      ),
+      (err) => {
+        if (err) {
+          console.error("Error writing readme.md:", err)
+        } else {
+          console.log("readme.md updated successfully.")
+        }
       }
-    })
+    )
   })
   .catch((error) => {
     console.error("Error downloading readme.md:", error)
